f7616390df97a42a3131b6eaaceec062e02f2060
karthik nambiar <karthiknambiar4u@gmail.com>
2023-04-21 19:25:39 +0530

    wrap forgot shit

--- drive/data_tester.py
+++ drive/data_tester.py
@@ -26,7 +26,7 @@ BLUE = Color('blue')
 RED = Color('red')
 PIXELS_PER_METER = 5
 N_STEP=5
-CROP_SIZE = 192
+CROP_SIZE = 320
 MAP_SIZE=320
 config = {'device': torch.device('cuda' if torch.cuda.is_available() else 'cpu'),
         'teacher_args' : {
@@ -79,18 +79,18 @@ class CoordConverter():
         uv = np.matmul(np.matmul(self._A, ROTATION_MATRIX), XYZ.T)
         uv /= uv[-1]
         uv = uv.T
-        print(uv, uv[:, 0:-1])
+        # print(uv, uv[:, 0:-1])
         return uv[:, 0:-1], #image_xy[:,0]
     
     def __call__(self, map_locations):
         teacher_locations = map_locations.detach().cpu().numpy()
-        teacher_locations = (teacher_locations + 1) * CROP_SIZE / 2
+        teacher_locations = (teacher_locations + 1) * 320 / 2
         N = teacher_locations.shape[0]
-        teacher_locations[:,:,1] = CROP_SIZE - teacher_locations[:,:,1]
-        teacher_locations[:,:,0] -= CROP_SIZE/2
+        teacher_locations[:,:,1] = 320 - teacher_locations[:,:,1]
+        teacher_locations[:,:,0] -= 320/2
         teacher_locations = teacher_locations / PIXELS_PER_METER
-        print('teacher_locations', teacher_locations)
-        teacher_locations[:,:,1] += self._fixed_offset
+        # print('teacher_locations', teacher_locations)
+        # teacher_locations[:,:,1] += self._fixed_offset
         teacher_locations = self._project_image_xy(np.reshape(teacher_locations, (N*N_STEP, 2)))
         teacher_locations = np.reshape(teacher_locations, (N,N_STEP,2))
         teacher_locations = torch.FloatTensor(teacher_locations)
@@ -106,6 +106,7 @@ def world_to_pixel (x, y, ox, oy, ori_ox, ori_oy, offset=(10+320+176, 192//2), s
     # print('loc', pixel_x, pixel_y)
     return np.array([pixel_x, pixel_y]) + offset
 def crop_birdview(birdview, dx=0, dy=0):
+    CROP_SIZE = 192
     x = 260 - CROP_SIZE // 2 + dx
     y = MAP_SIZE // 2 + dy
 
@@ -116,7 +117,7 @@ def crop_birdview(birdview, dx=0, dy=0):
     return birdview
 
 args = YamlConfig.from_nested_dicts(load_config('config/hound_straight.yaml'))
-env = lmdb.open('/home/moonlab/Documents/karthik/lbc/000')
+env = lmdb.open('/home/moonlab/Documents/karthik/lbc/001')
 pygame.init()
 pygame.font.init()
 display = pygame.display.set_mode(
@@ -145,7 +146,7 @@ with env.begin() as txn:
         display.blit(pygame.surfarray.make_surface(rgb_right.swapaxes(0, 1)), (800, 0))
         display.blit(pygame.surfarray.make_surface(np.zeros((320, 280))), (1600, 320))
         birdview = crop_birdview(bird_view)
-        bird_view = crop_birdview(visualize_birdview(bird_view))
+        bird_view = visualize_birdview(bird_view)
         display.blit(pygame.surfarray.make_surface(np.transpose(bird_view, (1, 0, 2))), (1600, 0))
         ox, oy, oz, ori_ox, ori_oy, vx, vy, vz, ax, ay, az, cmd, steer, throttle, brake, manual, gear, traffic_light  = measurement
         v_offset = 4
@@ -178,21 +179,22 @@ with env.begin() as txn:
         items.append('Traffic Light:   % 12s' % ('RED' if traffic_light == 1.0 else 'GREEN'))
         
         #birdview
-        birdview = np.reshape(birdview, (192, 192, 5))
+        # birdview = np.reshape(birdview, (192, 192, 5))
         birdview = birdview_transform(birdview)
         birdview = birdview[None, :].to(config['device'])
-        command = one_hot(torch.Tensor([cmd])).to(config['device'])
+        command = one_hot(torch.Tensor([0.0])).to(config['device'])
         speed = torch.Tensor([float(speed)]).to(config['device'])
         traffic = torch.Tensor([traffic_light]).to(config['device'])
-        
+        print(command)
         with torch.no_grad():
             _teac_location = teacher_net(birdview, speed, command, traffic)
         coord_converter = CoordConverter()
         teac_location = coord_converter(_teac_location)
-        print(teac_location.shape)
-        _teac_location = (_teac_location + 1) * (0.5 * CROP_SIZE)
+        # print(teac_location.shape)
+        _teac_location = (_teac_location + 1) * (0.5 * 192)/PIXELS_PER_METER
+        # print(_teac_location)
         for teac_loc in _teac_location[0]:
-            pygame.draw.rect(display, RED, pygame.Rect(teac_loc[0]+190//2+1600, 192+teac_loc[1]-70, 3, 3))
+            pygame.draw.rect(display, RED, pygame.Rect(teac_loc[0]*5+320//2+1600, 192+teac_loc[1], 3, 3))
         for teac_loc in teac_location[0]:
             pygame.draw.rect(display, RED, pygame.Rect(teac_loc[0], teac_loc[1], 3, 3))
         # print(_teac_location, teac_location)
@@ -212,6 +214,10 @@ with env.begin() as txn:
                     surface = _font_mono.render(item[0], True, (255, 255, 255))
                     display.blit(surface, (1608, 320+v_offset))
                     v_offset +=18
+            else:
+                surface = _font_mono.render(item, True, (255, 255, 255))
+                display.blit(surface, (1608, 320+v_offset))
+                v_offset +=18
             gap = 5
             n_step = 5
             ox, oy, oz, ori_ox, ori_oy  = measurement[:5]
@@ -229,10 +235,11 @@ with env.begin() as txn:
                 # else:
                 gt_loc.append([xx, yy])
                 dt +=5
+            # print('gt', np.array(gt_loc)*5)
             for loc in gt_loc:
                 pixel_y = loc[0]*5
                 pixel_x = loc[1]*5
-                pygame.draw.rect(display, BLUE, pygame.Rect(pixel_x+190//2+1600, 192+pixel_y-10, 3, 3))
+                pygame.draw.rect(display, BLUE, pygame.Rect(pixel_x+320//2+1600, 260+pixel_y-14, 3, 3))
             ROTATION_MATRIX = np.array([
                 [1, 0, 0, 0],
                 [0, 1, 0, 0],
@@ -261,8 +268,8 @@ with env.begin() as txn:
                 point_right = np.matmul(np.matmul(A, ROTATION_MATRIX), np.concatenate([np.matmul(EXTRINSIC_ROTATION_RIGHT,point), [1]]))
                 point_right /= point_right[-1]
                 #print('point_left', point_left)
-                if 0 < point_left[0] and point_left[0] < 800 and 0 < point_left[1] and  point_left[1] < 600:
-                    pygame.draw.rect(display, BLUE, pygame.Rect(point_left[0], point_left[1], 3, 3))
+                # if 0 < point_left[0] and point_left[0] < 800 and 0 < point_left[1] and  point_left[1] < 600:
+                #     pygame.draw.rect(display, BLUE, pygame.Rect(point_left[0], point_left[1], 3, 3))
                 #else: 
                     #print('XX')
 
